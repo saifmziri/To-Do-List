fb5cd7c900fa842f2f3a57283dda7672c1c39f40
saif saif <saefmziri10@gmail.com>
2026-07-13 20:07:53 +0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,11 @@ import Card from "./components/card/Card";
 import { useState, useEffect } from "react";
 import { TaskDataContext } from "./context/TaskDataContext";
 
-const DEFAULT_TASKS = [
-  {
-    id: 1,
-    title: "Sample Task",
-    description: "lorem ipsum dolor sit amet...",
-    isCompleted: false,
-  },
-  {
-    id: 2,
-    title: "OOP function",
-    description: "We are learning about OOP functions in JavaScript.",
-    isCompleted: true,
-  },
-  {
-    id: 3,
-    title: "Frontend Development",
-    description: "We are learning about frontend development.",
-    isCompleted: false,
-  },
-];
-
 function App() {
   // قراءة البيانات من الكاش فوراً عند تحميل التطبيق لمنع الـ Flash للبيانات القديمة
   const [taskData, setTaskData] = useState(() => {
     const storageData = localStorage.getItem("taskData");
-    return storageData ? JSON.parse(storageData) : DEFAULT_TASKS;
+    return storageData ? JSON.parse(storageData) : [];
   });
 
   // حفظ مركزي وحيد لكل التطبيق
